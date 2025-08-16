e34601757af1330ad6ed58ca9271c64ab8498090
Caleb <developer.caleb@proton.me>
2025-08-16 14:55:50 -0500

    better union mapper using infer

--- UnionMapper.ts
+++ UnionMapper.ts
@@ -1,29 +1,4 @@
 /**
- * Intended use is to make each key a subtype of the discriminator type,
- * and each value a specific mapping for that subtype
- * {
- *   "circle": {
- *     "radius": number;
- *   },
- *   "square": {
- *    "area": number;
- *   }
- * }
- */
-type UnionMapper<TDiscriminator> = TDiscriminator extends number | string ? Record<TDiscriminator, Record<string, any>> : never;
-type MaybeApplyUnionMapper<
-  Subtype,
-  DiscriminatorValue,
-  M extends UnionMapper<any>
-> = DiscriminatorValue extends keyof M ? Mapped<Subtype, M[DiscriminatorValue]> : Subtype;
-
-// applies a mapper to a specific type
-type Mapped<T, M extends Record<string, any>> = {
-  [K in keyof T]: K extends keyof M ? M[K] : T[K];
-};
-
-
-/**
     * Maps a union of types to a new union of types.
     * @param TUnion - The union
     * @param DiscriminatingKey - A key which is required to be common across all the subtypes in the union, and unique among them   
@@ -48,15 +23,13 @@ type Mapped<T, M extends Record<string, any>> = {
     *  are copied from the original union types.
 >;*/
 export type MapUnion<
-  TUnion,
-  DiscriminatingKey extends keyof TUnion,
-  TMapper extends UnionMapper<TUnion[DiscriminatingKey]>
-> = TUnion[DiscriminatingKey] extends string | number
-  ? {
-      [Discriminator in TUnion[DiscriminatingKey]]: MaybeApplyUnionMapper<
-        TUnion & { [K in DiscriminatingKey]: Discriminator },
-        Discriminator,
-        TMapper
-      >;
-    }[TUnion[DiscriminatingKey]]
+  T,
+  K extends keyof T,
+  Overrides extends Record<PropertyKey, object>
+> = T extends infer U
+  ? U extends Record<K, keyof Overrides>
+    ? Omit<U, keyof Overrides[U[K]]> & Overrides[U[K]]
+    : U
   : never;
+
+
